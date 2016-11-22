6c5459da81b84251c74a4019f2c6c77343e2b4fc
Binoy Patel <binoypatel14@gmail.com>
2016-11-21 23:23:16 -0500

    Added google oAuth and retrival of calendar events

--- android/app/src/main/java/com/smarts/MainApplication.java
+++ android/app/src/main/java/com/smarts/MainApplication.java
@@ -4,11 +4,18 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.modules.network.ReactCookieJarContainer;
+import com.facebook.stetho.Stetho;
+import okhttp3.OkHttpClient;
+import com.facebook.react.modules.network.OkHttpClientProvider;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import java.util.concurrent.TimeUnit;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new VectorIconsPackage()
       );
     }
@@ -34,4 +42,17 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
       return mReactNativeHost;
   }
+
+  public void onCreate() {
+      super.onCreate();
+      Stetho.initializeWithDefaults(this);
+      OkHttpClient client = new OkHttpClient.Builder()
+      .connectTimeout(0, TimeUnit.MILLISECONDS)
+      .readTimeout(0, TimeUnit.MILLISECONDS)
+      .writeTimeout(0, TimeUnit.MILLISECONDS)
+      .cookieJar(new ReactCookieJarContainer())
+      .addNetworkInterceptor(new StethoInterceptor())
+      .build();
+      OkHttpClientProvider.replaceOkHttpClient(client);
+}
 }
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,110 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import moment from 'moment';
 import {
   View,
+  StyleSheet,
+  TouchableHighlight,
   Text,
 } from 'react-native';
+import axios from 'axios';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+import { List, ListItem } from 'native-base';
+import { GOOGLE_API_KEY } from '../../config';
 
-export const Calendar = () => (
-  <Text>Calendar</Text>
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
+export class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calendarItems: [],
+    };
+
+    this.signIn = this.signIn.bind(this);
+  }
+
+  componentWillMount() {
+    GoogleSignin.hasPlayServices({ autoResolve: true }).then(() => {
+      GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+        iosClientId: '1060428883192-3ig74dfv5882k58a61m8pkqt1kgj83ej.apps.googleusercontent.com',
+      });
+    })
+    .catch((err) => {
+      console.log("Play services error", err.code, err.message);
+    });
+  }
+
+  signIn() {
+
+    GoogleSignin.signIn()
+      .then((user) => {
+        const startTime = moment.utc().startOf('day').format();
+        const endTime = moment.utc().endOf('day').format();
+        const url = `https://www.googleapis.com/calendar/v3/calendars/binoypatel14%40gmail.com/events?key=${GOOGLE_API_KEY}&timeMin=${startTime}&timeMax=${endTime}`;
+        axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        })
+          .then((response) => {
+            console.log(response.data.items);
+            this.setState({
+              calendarItems: response.data.items,
+            });
+          })
+          .catch((e) => {
+            console.log(e.message);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  // _signOut() {
+  //   GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+  //     console.log("Signed out");
+  //   })
+  //   .done();
+  // }
+
+  render() {
+    if(this.state.calendarItems.length !== 0) {
+      return (
+        <View>
+          <List
+            dataArray={this.state.calendarItems}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{moment(item.start.dateTime).format("LT")} - {moment(item.end.dateTime).format("LT")} : {item.summary}</Text>
+              </ListItem>
+            }>
+          </List>
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style={styles.container}>
+          <GoogleSigninButton
+            style={{width: 120, height: 48}}
+            size={GoogleSigninButton.Size.Icon}
+            color={GoogleSigninButton.Color.Light}
+            onPress={this.signIn}
+            >
+          </GoogleSigninButton>
+          <TouchableHighlight onPress={this.signIn}>
+            <Text>Sign in With Google</Text>
+          </TouchableHighlight>
+        </View>
+      );
+    }
+  }
+}
