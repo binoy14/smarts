8047d9e1d34614855071d6c48bc3d40bec4c9e4f
Binoy Patel <binoypatel14@gmail.com>
2016-11-28 15:23:25 -0500

    Adds ability to select calendar id

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,6 +4,8 @@ import {
   Text,
   ScrollView,
   StyleSheet,
+  Modal,
+  TouchableHighlight,
 } from "react-native";
 import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 import {
@@ -28,9 +30,14 @@ export default class Calendar extends Component {
     super(props);
     this.state = {
       calendarItems: [],
+      calendarIds: [],
+      selectedCalendarId: "",
+      modalVisible: false,
+      user: null,
     };
 
     this.signIn = this.signIn.bind(this);
+    this.onCalendarIdSelect = this.onCalendarIdSelect.bind(this);
   }
 
   componentWillMount() {
@@ -49,27 +56,53 @@ export default class Calendar extends Component {
   signIn() {
     GoogleSignin.signIn()
       .then((user) => {
-        const startTime = moment.utc().startOf('day').format();
-        const endTime = moment.utc().endOf('day').format();
-        const url = `https://www.googleapis.com/calendar/v3/calendars/binoypatel14%40gmail.com/events?key=${GOOGLE_API_KEY}&timeMin=${startTime}&timeMax=${endTime}`;
+        this.setState({user});
+        const url = `https://www.googleapis.com/calendar/v3/users/me/calendarList?key=${GOOGLE_API_KEY}`;
         axios.get(url, {
           headers: {
             Authorization: `Bearer ${user.accessToken}`,
           },
         })
-          .then((response) => {
-            this.setState({
-              calendarItems: response.data.items,
-            });
-          })
-          .catch((e) => {
-            console.log(e.message);
+        .then((response) => {
+          console.log(response);
+          this.setState({
+            calendarIds: response.data.items,
+            modalVisible: true,
           });
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
       })
       .catch((err) => {
         console.log(err);
       });
   }
+
+  onCalendarIdSelect(calendarId) {
+    console.log(calendarId);
+    const {user} = this.state;
+    const startTime = moment.utc().startOf('day').format();
+    const endTime = moment.utc().endOf('day').format();
+    const encoded = encodeURIComponent(calendarId);
+    const url = `https://www.googleapis.com/calendar/v3/calendars/${encoded}/events?key=${GOOGLE_API_KEY}&timeMin=${startTime}&timeMax=${endTime}`;
+    axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+      .then((response) => {
+        this.setState({
+          calendarItems: response.data.items,
+          modalVisible: false,
+          calendarIds: [],
+        });
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }
+
   _signOut() {
     GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
       console.log("Signed out");
@@ -78,14 +111,47 @@ export default class Calendar extends Component {
   }
 
   render() {
-    const {calendarItems} = this.state;
+    const {calendarItems, calendarIds} = this.state;
+    console.log(calendarIds, calendarItems);
+    if(calendarIds.length !== 0) {
+      return <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22}}>
+            <Text>Select Calendar to Display</Text>
+            <List>
+              {
+                calendarIds.map((id) => {
+                  return <ListItem
+                    key={id.etag}
+                    title={id.summary}
+                    onPress={() => {this.onCalendarIdSelect(id.id)}}
+                    description={id.description}
+                  />
+                })
+              }
+            </List>
+         </View>
+        </Modal>
+    }
+
+
+
     if(calendarItems.length !== 0) {
       return (
         <ScrollView>
         {calendarItems.map((item, i) => {
           const num = Math.floor(Math.random() * 5) + 1;
-          const location = <Text>Location: {item.location}</Text>
-          const description = <Text>Description: {item.description}</Text>
+          let location, description;
+          if(item.location) {
+            location = <Text>Location: {item.location}</Text>
+          }
+          if(item.description) {
+            description = <Text>Description: {item.description}</Text>
+          }
           const startTime = <Text>Start Time: {moment(item.start.dateTime).format("LT")}</Text>
           const endTime = <Text>End Time: {moment(item.end.dateTime).format("LT")}</Text>
           switch (num) {
